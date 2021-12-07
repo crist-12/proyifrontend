626a7ecd9db4d31497cfa8f7bbbe705c9e4cd31b
Christopher Ortiz <58318100+crist-12@users.noreply.github.com>
2021-12-07 11:40:25 -0600

    cambio

--- src/components/New/index.js
+++ src/components/New/index.js
@@ -5,20 +5,41 @@ import { PrimaryButton } from '../UI/Button';
 import { useEffect, useState } from 'react'
 import { privateAxios } from '../../store/utils/Axios';
 import { useDispatch} from 'react-redux';
+import axios from 'axios'
 
 import "./index.css";
 const New = ()=>{
   const [txtTitle, setTxtTitle] = useState("");
   const [txtMessage, setTxtMessage] = useState("");
   const [txtCorreo, setTxtCorreo] = useState("");
-  const [imagen, setArchivo] = useState(null);
+  const [file, setFile] = useState();
+  const [fileName, setFileName] = useState("");
+  const [ruta, setRuta] = useState("");
   const dispatch = useDispatch();
 
-  useEffect(()=>{
-    setArchivo(imagen)
-  },[imagen]);
+ 
 
+  const saveFile = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setFile(e.target.files[0]);
+    setFileName(e.target.files[0].name);
+  };
 
+  const uploadFile = async (e) => {
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("fileName", fileName);
+    try {
+      const res = await axios.post(
+        "http://localhost:3000/upload",
+        formData
+      )
+      setRuta(res.data.message);
+    } catch (ex) {
+      console.log(ex);
+    }
+  };
   
   const mailvalid = (mail) => {
     return RegExp(/^(([^<>()\[\]\\.,;:\s@”]+(\.[^<>()\[\]\\.,;:\s@”]+)*)|(“.+”))@((\[[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}])|(([a-zA-Z\-0–9]+\.)+[a-zA-Z]{2,}))$/).test(mail);
@@ -41,10 +62,12 @@ const New = ()=>{
           title: txtTitle,
           message: txtMessage,
           to: txtCorreo,
+          ruta: ruta
         }
       )
       .then(({data})=>{
         console.log(data);
+        console.log("Agregamos uno nuevo :)")
         dispatch(
           {
             type:"SAVE_FETCH_SUCCESS",
@@ -78,8 +101,6 @@ const New = ()=>{
       setTxtCorreo(e.target.value);
     } else if (e.target.name === "txtMessage") {
       setTxtMessage(e.target.value);
-    } else if (e.target.name === "imagen") {
-      setArchivo(e.target.value);
     }else {
       setTxtTitle(e.target.value);
     }
@@ -109,14 +130,8 @@ const New = ()=>{
           onChange={onChangeHandler}
           name="txtCorreo"
         />
-        <TextBox
-          label="Hacia"
-          type="file"
-          value={imagen}
-          placeholder="Imagen"
-          onChange={onChangeHandler}
-          name="imagen"
-        />
+              <input type="file" onChange={saveFile} />
+        <button onClick={uploadFile}>Upload</button>
         <div style={{ width: "100%", padding: '0.5em', marginTop: '1em' }}>
           <PrimaryButton onClick={onBtnClick}>Guardar</PrimaryButton>
         </div>
